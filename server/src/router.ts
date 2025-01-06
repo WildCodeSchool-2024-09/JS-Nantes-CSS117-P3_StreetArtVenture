45495d142ed4abf18c086f720d9246616b1cf323
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-06 10:23:51 +0100

    ✨: created findArtPiecesAround route

--- server/src/router.ts
+++ server/src/router.ts
@@ -7,11 +7,14 @@ const router = express.Router();
 /* ************************************************************************* */
 
 // Define item-related routes
+import artActions from "./modules/art/artActions";
 import itemActions from "./modules/item/itemActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+router.get("/art/findArtPiecesAround", artActions.browseAround);
 
 /* ************************************************************************* */
 
