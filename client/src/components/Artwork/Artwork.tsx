3f87f2aa2f49c9965140e1b7e02909f8ea6a7339
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-06 21:03:03 +0100

    css pour la barre de selection des villes

--- client/src/components/Artwork/Artwork.tsx
+++ client/src/components/Artwork/Artwork.tsx
@@ -1,6 +1,8 @@
 import "./Artwork.css";
 
-import backgroundimage from "../../assets/image/background-grey.jpg";
+import backgroundimage from "/background-grey.jpg";
+
+import traith1 from "/trait-h1-artwork.tsx.png";
 
 function Artwork() {
   return (
@@ -14,6 +16,35 @@ function Artwork() {
 
         <div className="white-card">
           <h1>Galerie d'oeuvre</h1>
+          <img
+            className="traith1"
+            src={traith1}
+            alt="background gray if from lighter to darker"
+          />
+
+          <select className="city" name="city">
+            <option className="select-city" value="nantes">
+              Nantes
+            </option>
+            <option className="select-city" value="paris">
+              Paris
+            </option>
+            <option className="select-city" value="strasbourd">
+              Strasbourg
+            </option>
+            <option className="select-city" value="toulouse">
+              Toulouse
+            </option>
+            <option className="select-city" value="marseille">
+              Marseille
+            </option>
+            <option className="select-city" value="nice">
+              Nice
+            </option>
+            <option className="select-city" value="clermond-ferrand">
+              Clermond-Ferrand
+            </option>
+          </select>
         </div>
       </section>
     </>
