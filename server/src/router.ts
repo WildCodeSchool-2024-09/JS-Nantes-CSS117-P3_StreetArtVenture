9a15806a7f950750e4ae3486723f0882637afeb2
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-08 11:10:11 +0100

    ✨: création endpoint permettant de récuperer les différentes villes des utilisateurs de la bdd, récuperer le classement, et le classement d'un utilisateur selon son id

--- server/src/router.ts
+++ server/src/router.ts
@@ -8,10 +8,15 @@ const router = express.Router();
 
 // Define item-related routes
 import itemActions from "./modules/item/itemActions";
+import userActions from "./modules/item/leaderboard/leaderboardActions";
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+router.get("/leaderboard/getCities", userActions.getCities);
+router.get("/leaderboard/getLeaderboard", userActions.getLeaderboard);
+router.get("/leaderboard/getUserData/:id", userActions.getUserData);
 
 /* ************************************************************************* */
 
