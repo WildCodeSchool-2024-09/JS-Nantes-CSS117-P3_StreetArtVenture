2569ca305853cf4be21846305c806c65664afd84
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-08 11:38:59 +0100

    first commit of the backend

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,6 +4,7 @@ const router = express.Router();
 
 /* ************************************************************************* */
 // Define Your API Routes Here
+
 /* ************************************************************************* */
 
 // Define item-related routes
@@ -12,6 +13,10 @@ import itemActions from "./modules/item/itemActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
+import { browse } from "./modules/art_piece/artPieceActions";
+
+router.get("/api/art", browse);
 
 /* ************************************************************************* */
 
