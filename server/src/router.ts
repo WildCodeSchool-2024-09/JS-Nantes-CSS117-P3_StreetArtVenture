35b3ccccbd857f83d67220e2386aa4cb82cbb4a3
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-09 11:50:28 +0100

    more test

--- server/src/router.ts
+++ server/src/router.ts
@@ -14,20 +14,6 @@ import leaderboardActions from "./modules/leaderboard/leaderboardActions";
 
 console.warn("test");
 
-router.get("/api/items", itemActions.browse);
-router.get("/api/items/:id", itemActions.read);
-router.post("/api/items", itemActions.add);
-router.post("/user/verify", userActions.verifyUser);
-router.post("/user/verifyToken", userActions.verifyToken);
-
-router.get("/leaderboard/getCities", leaderboardActions.getCities);
-router.get("/leaderboard/getLeaderboard", leaderboardActions.getLeaderboard);
-router.get("/leaderboard/getUserData/:id", leaderboardActions.getUserData);
-
-import { browse } from "./modules/art_piece/artPieceActions";
-
-router.get("/api/art", browse);
-
 /* ************************************************************************* */
 
 export default router;
