78386d798b75cc461829edd08ac566910b71ead0
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-09 11:51:14 +0100

    remove tests

--- server/src/router.ts
+++ server/src/router.ts
@@ -12,7 +12,19 @@ import itemActions from "./modules/item/itemActions";
 import leaderboardActions from "./modules/leaderboard/leaderboardActions";
 import userActions from "./modules/user/userActions";
 
-console.warn("test");
+router.get("/api/items", itemActions.browse);
+router.get("/api/items/:id", itemActions.read);
+router.post("/api/items", itemActions.add);
+router.post("/user/verify", userActions.verifyUser);
+router.post("/user/verifyToken", userActions.verifyToken);
+
+router.get("/leaderboard/getCities", leaderboardActions.getCities);
+router.get("/leaderboard/getLeaderboard", leaderboardActions.getLeaderboard);
+router.get("/leaderboard/getUserData/:id", leaderboardActions.getUserData);
+
+import { browse } from "./modules/art_piece/artPieceActions";
+
+router.get("/api", browse);
 
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
