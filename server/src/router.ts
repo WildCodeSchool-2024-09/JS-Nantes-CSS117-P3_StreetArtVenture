e825b68635ba98391b349792b065fab38419cd59
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-09 14:54:01 +0100

    final changes V2

--- server/src/router.ts
+++ server/src/router.ts
@@ -4,7 +4,6 @@ const router = express.Router();
 router.use(express.json());
 /* ************************************************************************* */
 // Define Your API Routes Here
-
 /* ************************************************************************* */
 
 import artActions from "./modules/art/artActions";
@@ -15,12 +14,15 @@ import userActions from "./modules/user/userActions";
 router.get("/api/items", itemActions.browse);
 router.get("/api/items/:id", itemActions.read);
 router.post("/api/items", itemActions.add);
+
 router.post("/user/verify", userActions.verifyUser);
 router.post("/user/verifyToken", userActions.verifyToken);
 
 router.get("/leaderboard/getCities", leaderboardActions.getCities);
 router.get("/leaderboard/getLeaderboard", leaderboardActions.getLeaderboard);
 router.get("/leaderboard/getUserData/:id", leaderboardActions.getUserData);
+
+router.get("/art/findArtPiecesAround", artActions.browseAround);
 
 import { browse } from "./modules/art_piece/artPieceActions";
 
