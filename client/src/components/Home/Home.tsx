0b5d67b9bc48258f57246267a0f887570e6af7d3
GaeBolga <benjamin.lefort@mail.com>
2025-01-12 16:37:29 +0100

    css and html closer to what was expected, work still in progress

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,15 +1,17 @@
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 export const Home: React.FC = () => {
+  const navigate = useNavigate();
   return (
     <main>
-      <h1 className="montserrat background-image">
-        Chasses les œuvres de Street-Art de ta ville !
-      </h1>
-      <article className="flex">
-        <div className="content-wrapper">
-          <h2 className="bangers-regular center">LE JEU</h2>
-          <p className="montserrat ">
+      <figure className="head-img">
+        <img className="head-img" src="./images/IMAGE_HOMEPAGE.jpg" alt="" />
+      </figure>
+      <article className="flex-article ">
+        <div className="content-wrapper ">
+          <h2 className="bangers-regular  center title-size">LE JEU</h2>
+          <p className="home-p --primary-font  ">
             Explore ta ville et découvre ses trésors cachés de street art !
             Accède à une carte interactive, trouve des œuvres près de toi et
             explore la ville pour les photographier et les valider grâce à la
@@ -18,7 +20,11 @@ export const Home: React.FC = () => {
             disparition. Grimpe dans le classement et deviens un expert du
             street art !
           </p>
-          <button type="button" className="green-button montserrat">
+          <button
+            type="button"
+            onClick={() => navigate("/Connexion")}
+            className="home-button home-green-button home-p montserrat "
+          >
             JOUER
           </button>
         </div>
@@ -28,15 +34,17 @@ export const Home: React.FC = () => {
           alt="la carte de nantes avec des points d'intérêts"
         />
       </article>
-      <article className="black-background flex">
+      <article className="dark-background flex-article">
         <img
           className="home-img"
           src="homePageDiscovery.jpg"
           alt="street art représentant un enfant ouvrant un rideau sur le mur pour découvrir de nouveaux tags"
         />
         <div className="content-wrapper">
-          <h2 className="bangers-regular center white">LES OEUVRES</h2>
-          <p className="montserrat white">
+          <h2 className="bangers-regular center white title-size">
+            LES OEUVRES
+          </h2>
+          <p className="--primary-font white  home-p">
             Explore toutes les œuvres de street art référencées, qu’elles soient
             encore visibles ou disparues. Plonge dans une galerie interactive
             qui répertorie chaque création avec ses détails, son emplacement et
@@ -44,22 +52,39 @@ export const Home: React.FC = () => {
             passés et présents, et enrichis ta connaissance du street art local
             en un seul clic.
           </p>
-          <button type="button" className="green-button montserrat">
+          <button
+            type="button"
+            onClick={() => navigate("/gallery")}
+            className="home-button home-green-button montserrat home-p"
+          >
             DECOUVRIR
           </button>
         </div>
       </article>
-      <article>
-        <h2 className="bangers-regular center">le classement</h2>
-        <p className="montserrat ">
-          Consulte le classement des explorateurs et mesure-toi aux autres
-          joueurs ! Suis ta progression, compare ton score avec celui des autres
-          joueurs, et repousse tes limites pour entrer dans le top. Qui sera le
-          grand champion de la découverte urbaine ?
-        </p>
-        <button type="button" className="green-button montserrat">
-          Voir le classement !
-        </button>
+      <article className="green-background flex-article">
+        <div className="content-wrapper">
+          <h2 className="bangers-regular center title-size white">
+            le classement
+          </h2>
+          <p className="--primary-font white home-p">
+            Consulte le classement des explorateurs et mesure-toi aux autres
+            joueurs ! Suis ta progression, compare ton score avec celui des
+            autres joueurs, et repousse tes limites pour entrer dans le top. Qui
+            sera le grand champion de la découverte urbaine ?
+          </p>
+          <button
+            type="button"
+            onClick={() => navigate("/leaderboard")}
+            className="home-button  white-button montserrat green-button-but-white-instead home-p"
+          >
+            Voir le classement !
+          </button>
+        </div>
+        <img
+          className="home-img"
+          src="images/leaderboardHome.jpg"
+          alt="street art représentant un enfant ouvrant un rideau sur le mur pour découvrir de nouveaux tags"
+        />
       </article>
     </main>
   );
