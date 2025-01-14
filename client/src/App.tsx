1783e23dc6e888189623d4356f8a157124d76086
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2025-01-14 16:47:38 +0100

    new footer ok

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import "./global.css";
 import Header from "./components/Header/Header";
 import "./App.css";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 }
