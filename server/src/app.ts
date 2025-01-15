7fde5f6f067518787c40c7a9ac297e99a5adfcfb
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-15 11:43:42 +0100

    🔧: Config env in app instead

--- server/src/app.ts
+++ server/src/app.ts
@@ -24,6 +24,10 @@ if (process.env.CLIENT_URL != null) {
   app.use(cors({ origin: [process.env.CLIENT_URL] }));
 }
 
+import dotenv from "dotenv";
+
+dotenv.config({ path: "./server/.env" });
+
 // If you need to allow extra origins, you can add something like this:
 
 /*
