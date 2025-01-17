babcc770447580de54c9e175ac88738a7e94c4ec
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-17 10:15:42 +0100

    add lost components and add background image and signalement text for this components

---
+++ client/src/components/lost/Lost.tsx
@@ -0,0 +1,13 @@
+import "./lost.css";
+
+function Lost() {
+  return (
+    <>
+      <section className="lost-page-background">
+        <h1 className="signalement-text">Signalement</h1>
+      </section>
+    </>
+  );
+}
+
+export default Lost;
