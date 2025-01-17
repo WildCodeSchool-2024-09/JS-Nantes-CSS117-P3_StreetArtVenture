efef6eb300d139cf1796e2a62854a9c1b467366c
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-17 12:25:46 +0100

    modification lost.tsx and lost.css add signalement and consultez les signalements des oeuvres d'art disparues and green card with oeuvre signalée and titre du street art

--- client/src/components/lost/Lost.tsx
+++ client/src/components/lost/Lost.tsx
@@ -1,10 +1,29 @@
 import "./lost.css";
-
+import traith1 from "/trait-h1-artwork.tsx.png";
 function Lost() {
   return (
     <>
       <section className="lost-page-background">
-        <h1 className="signalement-text">Signalement</h1>
+        <section className="signalement-container">
+          <h1 className="signalement-text">Signalement</h1>
+          <span className="circle">
+            <p>2</p>
+          </span>
+        </section>
+        <section className="works-container">
+          <h2 className="works-consulting">
+            Consultez les signalements d'oeuvres disparues
+          </h2>
+          <img
+            className="traith1"
+            src={traith1}
+            alt="background gray if from lighter to darker"
+          />
+          <section className="block-green-container">
+            <p className="reported-work">Oeuvre-signalée</p>
+            <p className="title-street-art">Titre du street art</p>
+          </section>
+        </section>
       </section>
     </>
   );
