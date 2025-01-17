8290ac8fd5bd251ab4d0c5d3cfed2c90f2462389
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-17 17:02:45 +0100

    create bloc green oeuvre signaléé and oeuvre comparee and scroll

--- client/src/components/lost/Lost.tsx
+++ client/src/components/lost/Lost.tsx
@@ -1,5 +1,6 @@
 import "./lost.css";
 import traith1 from "/trait-h1-artwork.tsx.png";
+
 function Lost() {
   return (
     <>
@@ -12,16 +13,24 @@ function Lost() {
         </section>
         <section className="works-container">
           <h2 className="works-consulting">
-            Consultez les signalements d'oeuvres disparues
+            Consultez les signalements d'œuvres disparues
           </h2>
           <img
             className="traith1"
             src={traith1}
-            alt="background gray if from lighter to darker"
+            alt="Background gray gradient from lighter to darker"
           />
-          <section className="block-green-container">
-            <p className="reported-work">Oeuvre-signalée</p>
-            <p className="title-street-art">Titre du street art</p>
+          <section className="block-green-reported-work">
+            <section className="block-green-container-reported-work">
+              <p className="reported-work">Oeuvre signalée</p>
+              <p className="title-street-art">Titre street art</p>
+              <p className="coordinates-gps">Coordonnées GPS</p>
+            </section>
+          </section>
+          <section className="block-compared-artwork-lost">
+            <section className="block-green-container-compared-artwork-lost">
+              <p className="compared-work">Oeuvre comparée</p>
+            </section>
           </section>
         </section>
       </section>
