f4fb151525399843b3a995a0d1de58a1709e23f0
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-20 16:40:29 +0100

    🏷️: create types for JWT

--- server/src/types/express/index.d.ts
+++ server/src/types/express/index.d.ts
@@ -1,14 +1,10 @@
 import type { ArtPiece } from "./artPiece";
-
+import type { JWTPayload } from "./auth";
 declare global {
   namespace Express {
     export interface Request {
       art_piece: ArtPiece;
-      /* ************************************************************************* */
-      // Add your custom properties here, for example:
-      //
-      // user?: { ... }
-      /* ************************************************************************* */
+      auth?: JWTPayload;
     }
   }
 }
