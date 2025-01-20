c49dd6a3dc491d974424067357a04674eeb88a81
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-20 16:04:44 +0100

    make the css for desktop

--- client/src/components/lost/Lost.tsx
+++ client/src/components/lost/Lost.tsx
@@ -1,5 +1,5 @@
 import "./lost.css";
-import traith1 from "/trait-h1-artwork.tsx.png";
+import traith1lost from "/trait-h1-artwork.tsx.png";
 
 function Lost() {
   return (
@@ -16,20 +16,22 @@ function Lost() {
             Consultez les signalements d'œuvres disparues
           </h2>
           <img
-            className="traith1"
-            src={traith1}
+            className="trait-h1-lost"
+            src={traith1lost}
             alt="Background gray gradient from lighter to darker"
           />
-          <section className="block-green-reported-work">
-            <section className="block-green-container-reported-work">
-              <p className="reported-work">Oeuvre signalée</p>
-              <p className="title-street-art">Titre street art</p>
-              <p className="coordinates-gps">Coordonnées GPS</p>
+          <section className="block-green">
+            <section className="block-green-reported-work">
+              <section className="block-green-container-reported-work">
+                <p className="reported-work">Oeuvre signalée</p>
+                <p className="title-street-art">Titre street art</p>
+                <p className="coordinates-gps">Coordonnées GPS</p>
+              </section>
             </section>
-          </section>
-          <section className="block-compared-artwork-lost">
-            <section className="block-green-container-compared-artwork-lost">
-              <p className="compared-work">Oeuvre comparée</p>
+            <section className="block-compared-artwork-lost">
+              <section className="block-green-container-compared-artwork-lost">
+                <p className="compared-work">Oeuvre comparée</p>
+              </section>
             </section>
           </section>
         </section>
