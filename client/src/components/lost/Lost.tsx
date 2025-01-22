9901a46bd337b91f37c8b4328740e3ca2b64e4de
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-22 13:06:03 +0100

    modification css reponsive and desktop button and row

--- client/src/components/lost/Lost.tsx
+++ client/src/components/lost/Lost.tsx
@@ -23,14 +23,32 @@ function Lost() {
           <section className="block-green">
             <section className="block-green-reported-work">
               <p className="compared-work">Oeuvre signalée</p>
+              <p className="title-street-art">Titre street art</p>
+              <p className="coordinates-gps">Coordonnées GPS</p>
+              <p className="description-work-lost">Description :</p>
+              <button className="btn-validation-lost" type="button">
+                Validation
+              </button>
+              <button className="btn-refusal-lost" type="button">
+                Refus
+              </button>
             </section>
             <section className="block-compared-artwork-lost">
               <section className="block-green-container-compared-artwork-lost">
                 <section className="block-green-container-reported-work">
                   <p className="reported-work">Oeuvre comparée</p>
-                  <p className="title-street-art">Titre street art</p>
-                  <p className="coordinates-gps">Coordonnées GPS</p>
+                  <button className="btn-next-lost" type="button">
+                    Suivant
+                  </button>
                 </section>
+              </section>
+              <section className="button-lost">
+                <button className="my-left-button-lost" type="button">
+                  <div className="arrow-left-lost arrow-lost" />
+                </button>
+                <button className="my-right-button-lost" type="button">
+                  <div className="arrow-right-lost arrow-lost" />
+                </button>
               </section>
             </section>
           </section>
