52d8276fbf00d6da1d3f84aa7399002397798d99
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-22 09:43:39 +0100

    🚚: rename page/components to be PascalCase

--- client/src/router/routes.tsx
+++ client/src/router/routes.tsx
@@ -2,11 +2,11 @@ import { Connexion } from "../components/Connexion/Connexion";
 import { Home } from "../components/Home/Home";
 
 import InscriptionForm from "../components/inscriptionForm/InscriptionForm";
-import LeaderboardPage from "../pages/leaderboard/leaderboardPage";
-import MapPage from "../pages/map/mapPage";
-import ProfilePage from "../pages/profile/profilePage";
+import LeaderboardPage from "../pages/leaderboard/LeaderboardPage";
+import MapPage from "../pages/map/MapPage";
 
-import Gallery from "../components/gallery/gallery";
+import Gallery from "../components/gallery/Gallery";
+import ProfilePage from "../pages/profile/ProfilePage";
 
 const routes = [
   {
