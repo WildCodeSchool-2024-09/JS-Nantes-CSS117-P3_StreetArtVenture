18c1dcb6bbaaf382f8feb08cab5a32fc7d141591
Idir CHEMMOUL <idir.chemmouL@gmail.com>
2025-01-23 14:41:53 +0100

    🚑️: use provider for context to prevent crash of app

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,14 +3,15 @@ import Header from "./components/Header/Header";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
+import { UserProvider } from "./context/UserContext";
 
 function App() {
   return (
-    <>
+    <UserProvider>
       <Header />
       <Outlet />
       <Footer />
-    </>
+    </UserProvider>
   );
 }
 
