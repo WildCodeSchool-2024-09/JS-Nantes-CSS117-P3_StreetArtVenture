d3b109695bb0516d73f07e728766ce950aefbf89
Julien-44100 <julienbotar44100@yahoo.com>
2025-01-30 09:29:57 +0100

    backend finish for lost.tsx

--- client/src/components/lost/LostType.ts
+++ client/src/components/lost/LostType.ts
@@ -1,5 +1,16 @@
+export type CoordinateI = {
+  x: string;
+  y: string;
+};
+
 export type LostI = {
   art_piece_id: number;
   user_id: number;
   timestamp: string;
+  user_name: string;
+  coordinates: { x: number; y: number };
+  art_piece_name: string;
+  city: string;
+  reported_img_path: string;
+  report_img_path: string;
 };
--- server/src/modules/reported_art_piece/reportedArtPieceRepository.ts
+++ server/src/modules/reported_art_piece/reportedArtPieceRepository.ts
@@ -14,7 +14,7 @@ class ReportedArtPieceRepository {
   async readAll() {
     // Execute the SQL SELECT query to retrieve all items from the "item" table
     const [rows] = await databaseClient.query<Rows>(
-      "SELECT r.picture_path, r.art_piece_id, r.user_id, r.timestamp, u.name AS user_name, a.coordinates, a.name AS art_piece_name FROM reported_art_piece r  INNER JOIN user u ON r.user_id = u.id  INNER JOIN art_piece a ON r.art_piece_id = a.id",
+      "SELECT r.picture_path AS report_img_path, a.picture_path AS reported_img_path, r.art_piece_id, r.user_id, r.timestamp, u.name AS user_name, a.coordinates, a.name AS art_piece_name FROM reported_art_piece r  INNER JOIN user u ON r.user_id = u.id  INNER JOIN art_piece a ON r.art_piece_id = a.id",
     );
     return rows as ReportedArtPIece[];
   }
