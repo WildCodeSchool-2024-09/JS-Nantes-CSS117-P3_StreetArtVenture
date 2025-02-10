cb7445e871818a5d6ac8febf321bc91516244520
ROMAIN LEMONNIER <lemonnier.romain13@gmail.com>
2025-02-10 16:00:45 +0100

    commit de test

--- server/src/router.ts
+++ server/src/router.ts
@@ -12,46 +12,12 @@ import userActions from "./modules/user/userActions";
 const router = express.Router();
 router.use(express.json());
 /* ************************** PUBLIC ACTIONS ************************** */
-
-router.post("/user/verify", userActions.hashPassword, userActions.verifyUser);
-
-/* ************************** LOGGED USERS ACTIONS ************************** */
-// router.use(authActions.verifyToken);
-router.get("/notifications/:id", notificationsActions.read);
-router.patch("/notifications/:id", notificationsActions.setRead);
-router.get("/user/reporting", reportedArtPieceActions.getUserSignalement);
-router.get("/art/findArtPiecesAround", artActions.browseAround);
-router.get("/art/getCities", artPieceActions.getCities);
-router.get("/leaderboard/getCities", leaderboardActions.getCities);
-router.get("/leaderboard/getLeaderboard", leaderboardActions.getLeaderboard);
-router.get("/leaderboard/getUserData/:id", leaderboardActions.getUserData);
-router.patch("/user/:id", userActions.patch); // TODO US-36
-
-/* ************************** ADMIN ACTIONS ************************** */
-// router.use(authActions.verifyAdmin);
-
-router.get(
-  "/leaderboard/admin/getLeaderboard",
-  leaderboardActions.getAdminLeaderboard,
-);
-router.patch("/art/artPieceValidation/:id", artActions.editArtPiece);
-router.patch("/reports/validate/:id", reportedArtPieceActions.validate);
-router.delete("/reports/refuse/:id", reportedArtPieceActions.refuse);
 router.get("/leaderboard/getCities", leaderboardActions.getCities);
 router.get("/leaderboard/getLeaderboard", leaderboardActions.getLeaderboard);
 router.get("/user/:id", userActions.read);
 router.get("/art/getCities", artPieceActions.getCities);
 router.get("/artPiece", artActions.readAll);
-router.get("/statistics/user", statisticsActions.getUserStatistics);
-router.get("/statistics/art_piece", statisticsActions.getArtPiecesStatistics);
-router.get("/statistics/player", statisticsActions.getPlayerStatistics);
-router.post("/api/upload", artActions.savePicture, artActions.multerAndSkully);
-router.post("/art/newArt", artActions.updateAccepted);
-
-/* ******************************************************************** */
-router.get("/art/latestArtPieceUnvelidated", artActions.unvalidatedArtPiece);
-router.delete("/art/artPieceDenied/:id", artActions.denyArtPiece);
-
+router.post("/user/verify", userActions.verifyUser);
 router.post("/user/verifyToken", userActions.verifyToken);
 router.post(
   "/user/registration",
